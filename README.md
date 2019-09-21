db1a9817c9084d29ce13d77e8ca095cf14188326
Jennifer <jennifer2018cs@gmail.com>
2019-09-22 01:30:39 +0930

    Added bootstrap theme, removed some files

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # mapbox
 A web-based application that implements:
-* A login page (Flask, ReactJS)
-* A map (Mapbox, ReactJS)
-* Polygon tools (Mapbox, Turf)
-* Measure an area on a map and return the size in square meter (Mapbox, Turf)
+* A login page (Bootstrap themed)
+* A map
+* Polygon tools
+* Measure an area on a map and return the size in square meter
 
-Built with Flask, ReactJS, Mapbox, Turf, PyProj.
+Built with Python, Flask, Bootstrap, ReactJS, Mapbox, Turf.
 
 ## Installation
 Install Python 3.7.4 and pip. Install flask using pip.
@@ -17,4 +17,16 @@ pip install flask
 ```bash
 python flaskapp.py
 ```
-Open localhost:5000 from a browser.
+Open [localhost:5000/login](localhost:5000/login) in a browser.
+
+Login with
+Username: admin
+Password: password
+
+Click the square button on the top-right corner of the map to draw polygon
+Calculated area size is shown on bottom-left corner
+Click trashcan button on the top-right corner of the map to delete polygon
+
+Reference:
+Flask tutorial: [https://youtu.be/MwZwr5Tvyxo](https://youtu.be/MwZwr5Tvyxo)
+Mapbox + Turf: [https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-draw/](https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-draw/)
--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from forms import LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'cb4857b56db25f4c2d0ae36edbea7902'
@@ -9,29 +9,16 @@ app.config['SECRET_KEY'] = 'cb4857b56db25f4c2d0ae36edbea7902'
 def home():
 	return render_template('home.html', title="Home")
 
-@app.route("/about")
-def about():
-	return render_template('about.html', title='About')
-
-@app.route("/register", methods=['GET','POST'])
-def register():
-	form = RegistrationForm()
-	if form.validate_on_submit():
-		flash(f'Account created for {form.username.data}!', 'success')
-		return redirect(url_for('home'))
-	return render_template('register.html', title='Register', form=form)
-
 @app.route("/login", methods=['GET','POST'])
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+		if form.username.data == 'admin' and form.password.data == 'password':
 			flash('You have been logged in!', 'success')
 			return redirect(url_for('home'))
 		else:
 			flash('Login Unsuccessful. Please check username and password', 'danger')
 	return render_template('login.html', title='Login', form=form)
-
 
 if __name__ == '__main__':
 	app.run(debug=True)
